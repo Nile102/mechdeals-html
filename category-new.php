b55ae505e0d7d63ac2b912be797a3c4ba5f00a6c
Dhirendra Maurya <dhirendrak@neuronsolutions.com>
2020-12-24 23:38:45 +0530

    category page html

--- category-new.php
+++ category-new.php
@@ -14,7 +14,7 @@
     
     <div id="wrapper" class="wrapper-fluid banners-effect-5">
     
-
+ 
      <?php include 'include/header.php' ?>
 
     <!-- Main Container  -->
@@ -143,6 +143,314 @@
           </div>
       </div>
     <!-- //Main Container -->
+
+  <div class="categoryList">
+    <div class="cateRow">
+      <div class="maincate">
+        <div class="name">
+          Air Conditioning <span>for RENAULT 5 (122_) 0.8 (1221, 1391)</span>
+        </div>
+       <div class="img">
+          <img src="image/category/category1.png" class="img-responsive">
+       </div>
+      </div>
+      <div class="subcate">
+        <ul class="list-inline top5">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+        </ul>
+        <!-- other category -->
+        <ul class="list-inline bottom3">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- xxxxx -->
+
+     <div class="cateRow">
+      <div class="maincate">
+        <div class="name">
+          Air Conditioning <span>for RENAULT 5 (122_) 0.8 (1221, 1391)</span>
+        </div>
+       <div class="img">
+          <img src="image/category/category1.png" class="img-responsive">
+       </div>
+      </div>
+      <div class="subcate">
+        <ul class="list-inline top5">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+        </ul>
+        <!-- other category -->
+        <ul class="list-inline bottom3">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- xxxxx -->
+
+     <div class="cateRow">
+      <div class="maincate">
+        <div class="name">
+          Air Conditioning <span>for RENAULT 5 (122_) 0.8 (1221, 1391)</span>
+        </div>
+       <div class="img">
+          <img src="image/category/category1.png" class="img-responsive">
+       </div>
+      </div>
+      <div class="subcate">
+        <ul class="list-inline top5">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate2.png" class="img-responsive">
+              </span>
+              <span class="name">Air Conditioner Compressor</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <span class="catelog">BestSeller</span>
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">Air Con Condenser</span>
+            </a>
+          </li>
+          <!-- xxxx -->
+        </ul>
+        <!-- other category -->
+        <ul class="list-inline bottom3">
+          <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+          <!-- xxxx -->
+           <li>
+            <a href="javascript:void(0);">
+              <span class="img">
+                <img src="image/category/subcate1.png" class="img-responsive">
+              </span>
+              <span class="name">
+                AC Evaporator
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- xxxxx -->
+  </div>
     
 
   <?php include 'include/footer.php' ?>
